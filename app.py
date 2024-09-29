41329e6da1f24b85967166bfb910974aaf75efa3
mstkyvz <mustafayvz-3211@hotmail.com>
2024-09-29 09:43:49 +0000

    add prompt

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 import requests
 import json
-
+import re
 url="http://0.0.0.0:8000/process"
 
 def image_to_base64(image):
@@ -13,12 +13,18 @@ def image_to_base64(image):
     byte_im = buf.getvalue()
     return base64.b64encode(byte_im).decode('utf-8')
 
+def extract_and_convert_to_json(input_string):
+    pattern = r'\{([^}]*)\}'
+    matches = re.findall(pattern, input_string)
+    combined = '{' + '}{'.join(matches) + '}'
+    
+    try:
+        json_output = json.loads(combined)
+        return json_output
+    except json.JSONDecodeError:
+        return "Geçerli bir JSON oluşturulamadı."
 
 st.set_page_config(layout="wide")
-
-
-
-
 
 
 hide_streamlit_style = """
@@ -44,13 +50,37 @@ st.markdown(
 col1, col2 = st.columns([1, 1])
 
 
+def preview_container(uploaded_file,description):
+    if uploaded_file is not None:
+            image = Image.open(uploaded_file)
+    else:
+            image = Image.open("empty.png")
+
+    if not description:
+            description="Açıklama"
+    st.markdown(
+            f"""
+            <div style="display:flex; flex-direction: column;  justify-content:  flex-start; border-radius: 5px; align-items: center; width: 100%; height: 600px; box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;">
+                <h2>Ön İzleme</h2>
+                <div style="width: 90%; height: 600px; overflow: hidden;">
+                    <img src="data:image/png;base64,{image_to_base64(image)}" style="width: 100%; height: 100%; object-fit: contain; "/>
+                </div>
+
+               <h5 style="margin:30px;" >{description}</h5> 
+
+            </div>
+            """,
+            unsafe_allow_html=True,
+        )
+
+
 with col1:
     with st.container():
         st.header("Görsel Yükle")
         uploaded_file = st.file_uploader("Görsel Yükle", type=["jpg", "jpeg", "png"])
         description = st.text_input("Açıklama")
         gonder = st.button("Gönder",use_container_width=True)
-
+        alert=st.empty()
 
     st.markdown(
         """
@@ -71,61 +101,47 @@ with col1:
     """,
         unsafe_allow_html=True,
     )
+    
 
 
 with col2:
-    
     if gonder:
         files = {'image': uploaded_file}
         data = {'text': description}
-        image = Image.open("temiz.png")
-        response = requests.post(url, files=files, data=data)
-        print(response.json())
-        output=response.json()['output']
-        base64_image = image_to_base64(image)
-        try:
-            output=output.replace("```json","").replace("`","")
-            output=json.loads(output)
-            urun_adi=output['urun_adi']
-            urun_aciklama=output['urun_detayli_aciklamasi']
-        except Exception as e:
-            print(e)
-            urun_adi=""
-            urun_aciklama=output
-        st.markdown(
-        f"""
-        <div style="display:flex; flex-direction: column; justify-content: flex-start; border-radius: 5px; align-items: center; width: 100%; height: 600px; box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;">
-        <h2>Çıktı</h2>
-        <h4>{urun_adi}</h4>
-            <div style="width: 90%; height: 600px; overflow: hidden;">
-                <img src="data:image/png;base64,{base64_image}" style="width: 100%; height: 100%; object-fit: contain;"/>
-            </div>
-        <h5 style="margin:30px;" >{urun_aciklama}</h5>    
-        </div>
-        """,
-        unsafe_allow_html=True,
-    )
-        
-    else:
-        if uploaded_file is not None:
-            image = Image.open(uploaded_file)
-        else:
-            image = Image.open("empty.png")
-
-        if not description:
-            description="Açıklama"
-        st.markdown(
+        print(uploaded_file)
+        if not uploaded_file:
+            alert.warning('Resimi Tekrar Yükleyin', icon="⚠️")
+            preview_container(uploaded_file,description)
+        else:    
+            image = Image.open("temiz.png")
+            response = requests.post(url, files=files, data=data)
+            output=response.json()['output']
+            base64_image = image_to_base64(image)
+            try:
+                output=output.replace("```json","").replace("`","")
+                print(output)
+                output=extract_and_convert_to_json(output)
+                print(output)
+                urun_adi=output['product_name']
+                urun_aciklama=output['product_detailed_description']
+            except Exception as e:
+                print(e)
+                urun_adi=""
+                urun_aciklama=output
+            st.markdown(
             f"""
-            <div style="display:flex; flex-direction: column;  justify-content:  flex-start; border-radius: 5px; align-items: center; width: 100%; height: 600px; box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;">
-                <h2>Preview</h2>
+            <div style="display:flex; flex-direction: column; justify-content: flex-start; border-radius: 5px; align-items: center; width: 100%; height: 600px; box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;">
+            <h2>Çıktı</h2>
+            <h4>{urun_adi}</h4>
                 <div style="width: 90%; height: 600px; overflow: hidden;">
-                    <img src="data:image/png;base64,{image_to_base64(image)}" style="width: 100%; height: 100%; object-fit: contain; "/>
+                    <img src="data:image/png;base64,{base64_image}" style="width: 100%; height: 100%; object-fit: contain;"/>
                 </div>
-
-               <h5 style="margin:30px;" >{description}</h5> 
-
+            <h5 style="margin:30px;" >{urun_aciklama}</h5>    
             </div>
             """,
             unsafe_allow_html=True,
         )
+
+    else:
+        preview_container(uploaded_file,description)
 
---
+++ gemma2-9b-4bit-api.py
@@ -0,0 +1,91 @@
+from fastapi import FastAPI, File, UploadFile, Form
+from fastapi.responses import JSONResponse
+import torch
+from PIL import Image
+from transformers import AutoModelForCausalLM,AutoModelForSequenceClassification,AutoTokenizer
+import io
+import os
+
+app = FastAPI()
+
+model = AutoModelForCausalLM.from_pretrained("AIDC-AI/Ovis1.6-Gemma2-9B",
+                                             torch_dtype=torch.bfloat16,
+                                             multimodal_max_length=8192,
+                                             trust_remote_code=True).cuda()
+text_tokenizer = model.get_text_tokenizer()
+visual_tokenizer = model.get_visual_tokenizer()
+
+
+model_ckpt = "papluca/xlm-roberta-base-language-detection"
+tokenizer_lang = AutoTokenizer.from_pretrained(model_ckpt)
+model_lang = AutoModelForSequenceClassification.from_pretrained(model_ckpt)
+
+
+
+def get_lang(text):
+    lang = tokenizer_lang(text, padding=True, truncation=True, return_tensors="pt")
+    with torch.no_grad():
+        logits = model_lang(**lang).logits
+
+    preds = torch.softmax(logits, dim=-1)
+    id2lang = model_lang.config.id2label
+    vals, idxs = torch.max(preds, dim=1)
+
+
+    first_key = id2lang[idxs[0].item()]
+    return first_key
+
+
+def read_text_file(file_name):
+    language_dir = 'prompt/language/'
+    file_path = os.path.join(language_dir, file_name)
+
+    if os.path.isfile(file_path):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            return file.read()
+    else:
+        fallback_path = os.path.join(language_dir, 'en.txt')
+        if os.path.isfile(fallback_path):
+            with open(fallback_path, 'r', encoding='utf-8') as file:
+                return file.read()
+
+            
+
+@app.post("/process/")
+async def process_image_and_text(image: UploadFile = File(...), text: str = Form(...)):
+    
+    
+    image_content = await image.read()
+    pil_image = Image.open(io.BytesIO(image_content))
+    lang=get_lang(text)
+    prompt = read_text_file(f"{lang}.txt")+text
+    
+    query = f'<image>\n{prompt}'
+    
+    prompt, input_ids, pixel_values = model.preprocess_inputs(query, [pil_image])
+    attention_mask = torch.ne(input_ids, text_tokenizer.pad_token_id)
+    input_ids = input_ids.unsqueeze(0).to(device=model.device)
+    attention_mask = attention_mask.unsqueeze(0).to(device=model.device)
+    pixel_values = [pixel_values.to(dtype=visual_tokenizer.dtype, device=visual_tokenizer.device)]
+    
+    
+    with torch.inference_mode():
+        gen_kwargs = dict(
+            max_new_tokens=1024,
+            do_sample=False,
+            top_p=None,
+            top_k=None,
+            temperature=None,
+            repetition_penalty=None,
+            eos_token_id=model.generation_config.eos_token_id,
+            pad_token_id=text_tokenizer.pad_token_id,
+            use_cache=True
+        )
+        output_ids = model.generate(input_ids, pixel_values=pixel_values, attention_mask=attention_mask, **gen_kwargs)[0]
+        output = text_tokenizer.decode(output_ids, skip_special_tokens=True)
+    
+    return JSONResponse(content={"output": output})
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
